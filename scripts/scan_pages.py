c733d81f31a19e6534d43b9866a6625498863e21
Unknown <blagopoluchie12@gmail.com>
2018-02-03 01:38:35 +0300

    reformat async syntax to Python > ver.3.5

--- scripts/scan_pages.py
+++ scripts/scan_pages.py
@@ -26,19 +26,17 @@ class MakeLists:
 		loop.run_until_complete(self.asynchronous(list_pages_for_scan, loop))
 		loop.close()
 
-	@asyncio.coroutine
-	def asynchronous(self, list_pages, loop):
+	async def asynchronous(self, list_pages, loop):
 		headers = {'user-agent': 'user:textworkerBot'}
 		sem = asyncio.Semaphore(limit_asynch_threads)
 		conn = aiohttp.TCPConnector(family=socket.AF_INET, verify_ssl=False)
-		with ClientSession(headers=headers, connector=conn, loop=loop) as session:
-			tasks = [asyncio.async(self.scan_pagehtml_for_referrors(sem, p, session)) for p in list_pages]
-			finished, unfinished = yield from asyncio.wait(tasks)
+		async with ClientSession(headers=headers, connector=conn, loop=loop) as session:
+			tasks = [asyncio.ensure_future(self.scan_pagehtml_for_referrors(sem, p, session)) for p in list_pages]
+			finished, unfinished = await asyncio.wait(tasks)
 			if len(unfinished):
 				print('have unfinished async tasks')
 
-	@asyncio.coroutine
-	def db_works(self, p):
+	async def db_works(self, p):
 		page_id, page_title, err_refs = p[0], p[1], p[2]
 		session.rollback()
 
@@ -55,8 +53,7 @@ class MakeLists:
 		session.add(Timecheck(page_id, time_current))
 		session.commit()
 
-	@asyncio.coroutine
-	def scan_pagehtml_for_referrors(self, sem, p, session):
+	async def scan_pagehtml_for_referrors(self, sem, p, session):
 		"""Сканирование страницы на ошибки"""
 		page_id, page_title = p[0], p[1]
 		url = 'https://ru.wikipedia.org/wiki/' + quote(page_title)
@@ -64,14 +61,14 @@ class MakeLists:
 		if page_title == 'None' or page_title is None:
 			print('!!!!!!!!!!!!!')
 
-		with (yield from sem):
+		async with sem:
 			retries = 0
 			while retries <= 5:
 				try:
-					response = yield from session.request('GET', url, params={"action": "render"}, )  # timeout=30
+					response = await session.request('GET', url, params={"action": "render"}, )  # timeout=30
 
 					if response.status == 200:
-						response_text = yield from response.text()
+						response_text = await response.text()
 						parsed_html = fromstring(response_text)
 						print(page_title)
 						page = ScanRefsOfPage(parsed_html)
@@ -80,12 +77,12 @@ class MakeLists:
 							errrefs
 						except:
 							pass
-						yield from self.db_works([page_id, page_title, errrefs])
+						await self.db_works([page_id, page_title, errrefs])
 						del page
 					elif response.status == 429:
 						# Too many requests
 						retries += 1
-						yield from asyncio.sleep(1)
+						await asyncio.sleep(1)
 					else:
 						print(page_title + ' response.status != 200')
 					response.close()
@@ -96,7 +93,7 @@ class MakeLists:
 					print('!!! Error. Page title: "%s"; url: %s; error: %r. Can will work on a next request.' % (
 						page_title, url, e))
 					retries += 1
-					yield from asyncio.sleep(1)
+					await asyncio.sleep(1)
 
 	def db_get_list_pages_for_scan(self):
 		return queryDB(session.query(Page.page_id, Page.title) \
