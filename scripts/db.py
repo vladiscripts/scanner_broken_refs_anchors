ef376bed303c7cc4d28c67aa328c516306d2bb42
Vlad <blagopoluchie12@gmail.com>
2018-06-26 23:16:17 +0300

    minor codestyling

--- scripts/db.py
+++ scripts/db.py
@@ -14,70 +14,69 @@ Session = sessionmaker(bind=db_engine)
 db_session = Session()
 
 
-# Объявление таблиц --------------
-# должно быть ниже объявления Base
-
 class Page(Base):
-	__tablename__ = 'pages'
-	page_id = Column(Integer, primary_key=True, unique=True)
-	title = Column(String, unique=True)
-	timeedit = Column(Integer)
-	wikilist = Column(String, index=True)
+    __tablename__ = 'pages'
+    page_id = Column(Integer, primary_key=True, unique=True)
+    title = Column(String, unique=True)
+    timeedit = Column(Integer)
+    wikilist = Column(String, index=True)
 
-	def __init__(self, page_id, title, timeedit):
-		self.page_id = page_id
-		self.title = title
-		self.timeedit = timeedit
-		fl = title[0:1].upper()
-		self.wikilist = '*' if re.match(r'[^АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ]', fl) else fl
+    def __init__(self, page_id, title, timeedit):
+        self.page_id = page_id
+        self.title = title
+        self.timeedit = timeedit
+        fl = title[0:1].upper()
+        self.wikilist = '*' if re.match(r'[^АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ]', fl) else fl
 
 
 class Timecheck(Base):
-	__tablename__ = 'timecheck'
-	page_id = Column(Integer, ForeignKey('pages.page_id'), primary_key=True, unique=True)
-	timecheck = Column(Integer)
+    __tablename__ = 'timecheck'
+    page_id = Column(Integer, ForeignKey('pages.page_id'), primary_key=True, unique=True)
+    timecheck = Column(Integer)
 
-	def __init__(self, page_id, timecheck):
-		self.page_id = page_id
-		self.timecheck = timecheck
+    def __init__(self, page_id, timecheck):
+        self.page_id = page_id
+        self.timecheck = timecheck
 
 
 class Ref(Base):
-	__tablename__ = 'erroneous_refs'
-	id = Column(Integer, primary_key=True)
-	page_id = Column(Integer, ForeignKey('pages.page_id'), index=True)
-	citeref = Column(String)
-	link_to_sfn = Column(String)
-	text = Column(String)
+    __tablename__ = 'erroneous_refs'
+    id = Column(Integer, primary_key=True)
+    page_id = Column(Integer, ForeignKey('pages.page_id'), index=True)
+    citeref = Column(String)
+    link_to_sfn = Column(String)
+    text = Column(String)
 
-	def __init__(self, page_id, citeref, link_to_sfn, text):
-		self.page_id = page_id
-		self.citeref = citeref
-		self.link_to_sfn = link_to_sfn
-		self.text = text
+    def __init__(self, page_id, citeref, link_to_sfn, text):
+        self.page_id = page_id
+        self.citeref = citeref
+        self.link_to_sfn = link_to_sfn
+        self.text = text
 
 
 class WarningTpls(Base):
-	__tablename__ = 'warnings'
-	page_id = Column(Integer, ForeignKey('pages.page_id'), ForeignKey('erroneous_refs.page_id'), primary_key=True, unique=True)
-	title = Column(String, unique=True)
+    __tablename__ = 'warnings'
+    page_id = Column(Integer, ForeignKey('pages.page_id'), ForeignKey('erroneous_refs.page_id'), primary_key=True, unique=True)
+    title = Column(String, unique=True)
 
-	def __init__(self, page_id, title):
-		self.page_id = page_id
-		self.title = title
+    def __init__(self, page_id, title):
+        self.page_id = page_id
+        self.title = title
 
 
 class Wikilists(Base):
-	__tablename__ = 'wikilists'
-	letter = Column(String, ForeignKey('pages.wikilist'), primary_key=True, unique=True)
-	title = Column(String)
+    __tablename__ = 'wikilists'
+    letter = Column(String, ForeignKey('pages.wikilist'), primary_key=True, unique=True)
+    title = Column(String)
 
-	def __init__(self, letter, title):
-		self.letter = letter
-		self.title = title
+    def __init__(self, letter, title):
+        self.letter = letter
+        self.title = title
+
 
 Base.metadata.create_all(db_engine)
 
+
 # Helpers
 def queryDB(query):
-	return db_session.execute(query).fetchall()
+    return db_session.execute(query).fetchall()
--- scripts/scan_pages.py
+++ scripts/scan_pages.py
@@ -11,53 +11,54 @@ from scripts.scan_refs_of_page import ScanRefsOfPage
 
 
 def do_scan():
-	"""Сканирование страниц на ошибки"""
-	s = open_requests_session()
+    """Сканирование страниц на ошибки"""
+    s = open_requests_session()
+    pages = db_get_list_pages_for_scan()
 
-	for p in db_get_list_pages_for_scan():
-		download_and_scan_page(s, p)
+    for p in pages:
+        download_and_scan_page(s, p)
 
 
 def download_and_scan_page(s, p):
-	id, title = p[0], p[1]
-	# if page_id != 273920:	continue  # For tests
-	r = s.get('https://ru.wikipedia.org/wiki/' + quote(title))
-	print(title + ':')
-	# page = ScanRefsOfPage(page_id, page_title)
-	page = ScanRefsOfPage(r.text)
-	scan_page([id, title, page.err_refs])
+    id, title = p[0], p[1]
+    # if page_id != 273920:	continue  # For tests
+    r = s.get('https://ru.wikipedia.org/wiki/' + quote(title))
+    print(title + ':')
+    # page = ScanRefsOfPage(page_id, page_title)
+    page = ScanRefsOfPage(r.text)
+    scan_page([id, title, page.err_refs])
 
 
 def scan_page(p):
-	"""Сканирование страниц на ошибки"""
-	page_id, page_title, err_refs = p[0], p[1], p[2]
-	db_session.rollback()
+    """Сканирование страниц на ошибки"""
+    page_id, page_title, err_refs = p[0], p[1], p[2]
+    db_session.rollback()
 
-	# Очистка db от списка старых ошибок
-	db_session.query(Ref).filter(Ref.page_id == page_id).delete()
-	db_session.query(Timecheck).filter(Timecheck.page_id == page_id).delete()
+    # Очистка db от списка старых ошибок
+    db_session.query(Ref).filter(Ref.page_id == page_id).delete()
+    db_session.query(Timecheck).filter(Timecheck.page_id == page_id).delete()
 
-	# Сканирование страниц на ошибки
-	for ref in err_refs:
-		db_session.add(Ref(page_id, ref['citeref'], ref['link_to_sfn'], ref['text']))
+    # Сканирование страниц на ошибки
+    for ref in err_refs:
+        db_session.add(Ref(page_id, ref['citeref'], ref['link_to_sfn'], ref['text']))
 
-	time_current = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-	db_session.add(Timecheck(page_id, time_current))
-	db_session.commit()
+    time_current = time.strftime('%Y%m%d%H%M%S', time.gmtime())
+    db_session.add(Timecheck(page_id, time_current))
+    db_session.commit()
 
 
 def db_get_list_pages_for_scan():
-	return queryDB(db_session.query(Page.page_id, Page.title) \
-				   .select_from(Page) \
-				   .outerjoin(Timecheck, Page.page_id == Timecheck.page_id) \
-				   .filter((Timecheck.timecheck.is_(None)) | (Page.timeedit > Timecheck.timecheck)))
+    return queryDB(db_session.query(Page.page_id, Page.title) \
+                   .select_from(Page) \
+                   .outerjoin(Timecheck, Page.page_id == Timecheck.page_id) \
+                   .filter((Timecheck.timecheck.is_(None)) | (Page.timeedit > Timecheck.timecheck)))
 
 
 def open_requests_session():
-	s = requests.Session()
-	s.headers.update({'User-Agent': 'user:textworkerBot'})
-	s.params.update({"action": "render"})
-	return s
+    s = requests.Session()
+    s.headers.update({'User-Agent': 'user:textworkerBot'})
+    s.params.update({"action": "render"})
+    return s
 
 # for test
 # page = ScanRefsOfPage('2091672', 'Марк Фульвий Флакк (консул 125 года до н. э.)')
