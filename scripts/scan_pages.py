2db7ba317e13b2e6647b7948a63b9fff86623679
Vlad <blagopoluchie12@gmail.com>
2018-07-19 21:24:52 +0300

    optimise

--- scripts/scan_pages.py
+++ scripts/scan_pages.py
@@ -17,44 +17,28 @@ def do_scan():
     pages = db_get_list_changed_pages()
     for pid, title in pages:
         # if id != 273920:	continue  # For tests
-        scan_results = scan_page(s, title)
+        print(title)
+        r = s.get(f'https://ru.wikipedia.org/wiki/{quote(title)}')
+        scan_results = ScanRefsOfPage(r.text)
         db_update_pagedata(pid, scan_results.err_refs)
     s.close()
 
 
-def scan_page(s, title):
-    """Сканирование страниц на ошибки"""
-    print(title)
-    r = s.get(f'https://ru.wikipedia.org/wiki/{quote(title)}')
-    scan_results = ScanRefsOfPage(r.text)
-    return scan_results
-
-
-def db_save_results(page_id, err_refs):
+def db_update_pagedata(page_id, err_refs):
     """Сохранение результатов сканирования в БД
     Очистка db от спискастарых ошибок в поддтаблицах автоматическая, с помощью ForeignKey ondelete='CASCADE'
     """
     db_session.rollback()
-    time_current = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-    db_session.query(PageWithSfn).filter(PageWithSfn.page_id == page_id).delete()
-    for ref in err_refs:
-        db_session.add(ErrRef(page_id, ref['citeref'], ref['link_to_sfn'], ref['text']))
-    db_session.add(Timecheck(page_id, time_current))
-    db_session.commit()
-
-
-def db_save_results_no_ForeignKeyCascadeDelete(page_id, err_refs):
-    """Сохранение результатов сканирования в БД"""
-    db_session.rollback()
-    time_current = time.strftime('%Y%m%d%H%M%S', time.gmtime())
     db_session.query(ErrRef).filter(ErrRef.page_id == page_id).delete()
     for ref in err_refs:
         db_session.add(ErrRef(page_id, ref['citeref'], ref['link_to_sfn'], ref['text']))
-    db_session.merge(Timecheck(page_id, time_current))
+    db_session.query(Timecheck).filter(Timecheck.page_id == page_id).delete()
+    db_session.add(Timecheck(page_id, time_current()))  # merge
+    # db_session.merge(Timecheck(page_id, time_current()))  # merge
     db_session.commit()
 
 
-def db_get_list_pages_for_scan():
+def db_get_list_changed_pages():
     pages = db_session.query(PageWithSfn.page_id, PageWithSfn.title) \
         .outerjoin(Timecheck, PageWithSfn.page_id == Timecheck.page_id) \
         .filter((Timecheck.timecheck.is_(None)) | (PageWithSfn.timelastedit > Timecheck.timecheck)).all()
@@ -66,6 +50,10 @@ def open_requests_session():
     s.headers.update({'User-Agent': 'user:textworkerBot'})
     s.params.update({"action": "render"})
     return s
+
+
+def time_current():
+    return time.strftime('%Y%m%d%H%M%S', time.gmtime())
 
 # for test
 # page = ScanRefsOfPage('2091672', 'Марк Фульвий Флакк (консул 125 года до н. э.)')
