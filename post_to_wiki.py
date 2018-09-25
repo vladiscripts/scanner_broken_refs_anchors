3a34185a5ed48a704ef59ddf533d29f9363065f4
Vlad <blagopoluchie12@gmail.com>
2018-09-25 14:49:56 +0300

    + pwb login as separately option (confusing that server sometime drop a post with cause `no login`)

--- post_to_wiki.py
+++ post_to_wiki.py
@@ -88,12 +88,20 @@ def remove_template():
     run(command, filename_list_to_remove_warning_tpl)
 
 
+def login():
+    command = f'{python_and_path} login -family:wikipedia'
+    code = subprocess.Popen(shlex.split(command)).wait()
+    if code != 0:
+        raise Exception
+
+
 if __name__ == '__main__':
     # Постинг списков и установка шаблонов в wiki
     if do_all_posts_simulate:
         do_post_wikilist_simulate = do_post_template_simulate = do_remove_template_simulate = True
 
     if do_all_post_to_wiki:
+        login()
         if do_post_wikilist:
             posting_list()
         if do_post_template:
