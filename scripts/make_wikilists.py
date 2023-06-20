74cf806acdbd699eac5a260246d7ddbee41a0a49
vladiscripts <nSm5wxm4>
2023-06-20 08:22:23 +0300

    '{{urlencode:%s}}' для профилактики ankors, сделанных глючными шаблонами, содержащих неэкранированные символы

--- scripts/make_wikilists.py
+++ scripts/make_wikilists.py
@@ -2,6 +2,7 @@
 # author: https://github.com/vladiscripts
 #
 import itertools
+import re
 from settings import *
 from scripts.db_models import PageWithSfn, ErrRef, db_session as s
 from scripts.make_listspages import file_savetext
@@ -20,9 +21,13 @@ def make_wikilists_by_page_ids():
         for i, page_refs in itertools.groupby(list(group_list), key=lambda g: g.page_id):
             page_refs = list(page_refs)
             p = page_refs[0]
-            refs_wikilinks = [r"[[#%s|%s]]" % (ref.link_to_sfn, ref.text) for ref in page_refs]
-            refs_entry = '* {pid} [[{t}]]:<br><section begin="{pid}" />{all_wikilinks}<section end="{pid}" />' \
-                .format(t=p.title.replace('_', ' '), pid=p.page_id, all_wikilinks=', '.join(refs_wikilinks))
+            refs_wikilinks = []
+            for ref in page_refs:
+                ankor = ref.link_to_sfn
+                if re.search(r'^[\w_-]+$', ref.link_to_sfn): ankor = '{{urlencode:%s}}' % ref.link_to_sfn
+                refs_wikilinks.append(f"[[#{ankor}|{ref.text}]]")
+            refs_entry = '* {pid} [[{title}]]:<br><section begin="{pid}" />{all_wikilinks}<section end="{pid}" />' \
+                .format(title=p.title.replace('_', ' '), pid=p.page_id, all_wikilinks=', '.join(refs_wikilinks))
             list_refs_entries.append(refs_entry)
 
         # Fill wikilists page
